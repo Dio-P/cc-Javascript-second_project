7ac19c0fe51b16254363b15f5dc8c3c9ee121482
Dionysios Papadopoulos <dionysiospapadopoulos@dionysioss-air.home>
2022-03-11 16:22:37 +0000

    all battles are now displayed properly where they should

--- client/src/App.js
+++ client/src/App.js
@@ -15,11 +15,19 @@ import './App.css';
 
 function App() {
 
+  const battles = entities[0]
   const battle = entities[0].geojson.geometry.coordinates[0]
-    
+  const battle1= entities[0].geojson.geometry.coordinates[0][0]
+  const purpleOptions = { color: 'purple' } 
+  
+  // const battles = {
+  //   entities.map(entity => entity.geojson.geometry.coordinates[0])
+  // }
 
   console.log("entities", entities);
+  console.log("battles", battles);
   console.log("battle", battle);
+  console.log("battle1", battle1[0]);
   return (
     <>
       <MapContainer center={[51.505, -0.09]} zoom={13}>
@@ -27,14 +35,20 @@ function App() {
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
+{  entities.map(entity => (
+  <Polygon pathOptions={purpleOptions} positions={entity.geojson.geometry.coordinates[0][0]
+    .map(coordinateArray=> coordinateArray.reverse())} />
+))
+}  
+{/* <Polygon pathOptions={purpleOptions} positions={battle} />
   <Marker position={[
--1.677834,
-53.762191
-]}>
+53.762191,
+-1.677834
+]}> */}
     {/* <Popup>
       A pretty CSS3 popup. <br /> Easily customizable.
     </Popup> */}
-  </Marker>
+  {/* </Marker> */}
 </MapContainer>
     {/* {entities.map(entity => entity.name)} */}
 
