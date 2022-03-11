dadb99fdd9bf4e2d6157bfe3e3cfe09c3558682d
Dionysios Papadopoulos <dionysiospapadopoulos@dionysioss-air.home>
2022-03-11 18:34:04 +0000

    I will not start working on locking the boundaries so the user not to be able to see the rest of the map besides the uk

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import {
   useMap
 } from 'react-leaflet'
 import entities from './data/test_data';
-import BattleView from './components/battleView';
+// import BattleView from './components/battleView';
 import MarkerElement from './components/markerElement';
 import './App.css';
 
@@ -27,6 +27,10 @@ function App() {
   console.log("battles", battles);////////////
   console.log("battle", battle);////////////
   console.log("battle1", battle1[0]);///////
+
+  // something that can be done if we like this functionality is to 
+  // have the market hidden by css and only the battlefield view displayed when zooming in
+  // we could have it vise versa when on zoom out view (the battlevield to be hidden or non rendering)
   
   return (
     <>
@@ -35,11 +39,14 @@ function App() {
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
+  {/* this under maps and displayes the battlefield. I tryed to have it in a different 
+  component, but for some reason it dosent like it */}
 {  entities.map(entity => (
   <Polygon pathOptions={purpleOptions} positions={entity.geojson.geometry.coordinates[0][0]
     .map(coordinateArray=> coordinateArray.reverse())} />
 ))
-}  
+} 
+ {/*the code bellow maps and renders the markers, this lives in a different component called MarkerElement  */}
 {entities.map((entity, index) => (
    <MarkerElement entity={entity} />
 ))}
