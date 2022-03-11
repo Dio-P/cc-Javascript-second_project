2ddfcafede64772e800547c36a60a4f6a2479e08
Dionysios Papadopoulos <dionysiospapadopoulos@dionysioss-air.home>
2022-03-11 15:48:02 +0000

    a variable with all the coordinates is desplayed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet'
+import {
+  Circle,
+  CircleMarker,
+  MapContainer,
+  Polyline,
+  Polygon,
+  Popup,
+  Rectangle,
+  TileLayer,
+  Marker
+} from 'react-leaflet'
 import entities from './data/test_data';
 import './App.css';
 
 function App() {
 
+  const battle = entities[0].geojson.geometry.coordinates[0]
+    
+
   console.log("entities", entities);
+  console.log("battle", battle);
   return (
     <>
       <MapContainer center={[51.505, -0.09]} zoom={13}>
@@ -13,10 +27,13 @@ function App() {
     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
   />
-  <Marker position={[51.505, -0.09]}>
-    <Popup>
+  <Marker position={[
+-1.677834,
+53.762191
+]}>
+    {/* <Popup>
       A pretty CSS3 popup. <br /> Easily customizable.
-    </Popup>
+    </Popup> */}
   </Marker>
 </MapContainer>
     {/* {entities.map(entity => entity.name)} */}
