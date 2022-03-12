401243b964ec10036b77efb429d8816bd3a0fe64
Chris <chris.gr.skinner@gmail.com>
2022-03-12 17:20:47 +0000

    amking the wikidata api -currently having cors error no access-contol-allow-origin - a bit stuck

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,50 @@
 // import MainContainer from './containers/MainContainer'
 import './App.css';
-import {getBattlefields} from './services/BattlefieldService'
+import {getBattlefields, getWikiEntry} from './services/BattlefieldService'
 import {useState, useEffect} from 'react'
 
 function App() {
-const [battleData, setBattleData] = useState(null)
-
-
-useEffect ( () => {
-  getBattlefields()
-  .then(data => setBattleData(data))
-  // .then(data => console.dir(data))
-
-} , [])
-
-if (!battleData) {return (
-  <div>
+  const [battleData, setBattleData] = useState(null)
+  const [WikiData, setWikiData] = useState(null)
+  
+  
+  useEffect ( () => {
+    getBattlefields()
+    .then(data => setBattleData(data))
+    // .then(data => console.dir(data))
+    
+  } , [])
+  useEffect ( () => {
+    getWikiEntry()
+    .then(data => setWikiData(data))
+    .then(data => console.dir(data))
+    // returning null
+  } , [])
+  
+  if (!battleData) {return (
+    <div>
     Loading...
   </div>
 ) } 
-
+// if (!WikiData) {return (
+  //   <div>
+  //     Loading...
+  //   </div>
+  // ) } 
+  
   const battlefieldNodes = battleData.map((listItem) => {
     return  (listItem.properties.name)
-    })
+  })
+
 
   return (
     <div className="App">
       <p>Hello World</p>
-      {console.log(battleData, "return statement")}
+      {console.log(battleData, "return battleData")}
+      {console.log(WikiData, "return WikiData")}
+
       {battlefieldNodes}
-      {/* <MainContainer /> */}
+
     </div>
   );
 }
--- client/src/services/BattlefieldService.js
+++ client/src/services/BattlefieldService.js
@@ -1,9 +1,6 @@
 const baseURL = 'http://127.0.0.1:5000/api/battlefields/';
 
 
-
-
-
 export const getBattlefields = () => {
     const result = fetch(baseURL)
     .then(res => res.json())
@@ -11,3 +8,10 @@ export const getBattlefields = () => {
     return result
 
 };
+
+export const getWikiEntry = () => {
+    const result = fetch('https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Battle%20of%20Barnet%201471&format=json ')
+    .then(res => res.json())
+
+    return result
+}
