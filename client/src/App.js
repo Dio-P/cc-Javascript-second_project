e23d11ccb984f1133f5b38137b1640548888375b
Dionysios Papadopoulos <dionysiospapadopoulos@dionysioss-air.home>
2022-03-12 19:51:35 +0000

    I think I found it

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { useCallback, useMemo, useState } from 'react'
+import { useCallback, useMemo, useState, useEffect } from 'react'
 import {
   Circle,
   CircleMarker,
@@ -17,18 +17,47 @@ import MarkerElement from './components/MarkerElement';
 import DiscreteSliderMarks from './components/DiscreteSliderMarks';
 import './App.css';
 
+const purpleOptions = { color: 'purple' } 
+
+function DisplayMap() {
+  return (
+    <>
+    <MapContainer center={[54.236, -4.54]} zoom={6}>
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    {/* this under maps and displayes the battlefield. I tryed to have it in a different 
+    component, but for some reason it dosent like it */}
+  {  entities.map(entity => (
+    <Polygon pathOptions={purpleOptions} positions={entity.geojson.geometry.coordinates[0][0]
+      .map(coordinateArray=> coordinateArray.reverse())} />
+  ))
+  } 
+   {/*the code bellow maps and renders the markers, this lives in a different component called MarkerElement  */}
+  {entities.map((entity, index) => (
+     <MarkerElement entity={entity} />
+  ))}
+  
+  </MapContainer>
+  <DiscreteSliderMarks />
+  </>
+
+  )
+}
+
 function App() {
-  // const [yearFilter, setYearFilter] = useState(2000);
+  const [yearFilter, setYearFilter] = useState(2000);
 
-  const battles = entities[0]////////////
-  const battle = entities[0].geojson.geometry.coordinates[0]//////////
-  const battle1= entities[0].geojson.geometry.coordinates[0][0]////////
-  const purpleOptions = { color: 'purple' } 
+  // const battles = entities[0]////////////
+  // const battle = entities[0].geojson.geometry.coordinates[0]//////////
+  // const battle1= entities[0].geojson.geometry.coordinates[0][0]////////
+  // const purpleOptions = { color: 'purple' } 
 
-  console.log("entities", entities);//////////
-  console.log("battles", battles);////////////
-  console.log("battle", battle);////////////
-  console.log("battle1", battle1[0]);///////
+  // console.log("entities", entities);//////////
+  // console.log("battles", battles);////////////
+  // console.log("battle", battle);////////////
+  // console.log("battle1", battle1[0]);///////
  
 
   // something that can be done if we like this functionality is to 
@@ -52,32 +81,46 @@ function App() {
 //   return (getYearOfBattle(battlefield) <= yearFilter)
 // })
 
-  return (
-    <>
-      <MapContainer center={[54.236, -4.54]} zoom={6}>
-  <TileLayer
-    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-  />
-  {/* this under maps and displayes the battlefield. I tryed to have it in a different 
-  component, but for some reason it dosent like it */}
-{  entities.map(entity => (
-  <Polygon pathOptions={purpleOptions} positions={entity.geojson.geometry.coordinates[0][0]
-    .map(coordinateArray=> coordinateArray.reverse())} />
-))
-} 
- {/*the code bellow maps and renders the markers, this lives in a different component called MarkerElement  */}
-{entities.map((entity, index) => (
-   <MarkerElement entity={entity} />
-))}
+// let entities = async() => await fetch("http://localhost:5050/api/battles").then(res=> res.json())
+// let data = null
 
-</MapContainer>
-<DiscreteSliderMarks />
+
+
+const entitiesTry = async() => {
+  try{
+  const data = await fetch("http://localhost:5050/api/battles");
+  const dataArray = await data.json();
+  console.log("dataArray", dataArray);
+  return dataArray
+  }catch(e){
+    console.error(e)
+
+  }
+}
+// getData()
+let entities = ()=>entitiesTry()
+
+console.log("entities2", entities);
+
+
+  useEffect(() => {
+    
+  }, []);
+
+  
+      
+  // console.log("getDataFromDb", getDataFromDb);
+
+  return (
+    <div>
+    <DisplayMap/>
+    
+      
 {/* changeYearValue={changeYearValue} */}
 
 
 
-    </>
+    </div>
   );
 }
 
--- server/server.js
+++ server/server.js
@@ -10,10 +10,10 @@ app.use(express.json())
 MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
 .then(client => {
 
-  const db = client.db('hotel');
-  const visitiorsCollection = db.collection('visitors');
-  const visitiorsRouter = createRouter(visitiorsCollection)
-  app.use('/api/hotel', visitiorsRouter);
+  const db = client.db('battles_britain');
+  const battlesCollection = db.collection('battles');
+  const battlesRouter = createRouter(battlesCollection)
+  app.use('/api/battles', battlesRouter);
 })
   app.listen(5050, function(){
     console.log(`app listening on port ${this.address().port}`);
