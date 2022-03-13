3d0e5bbab5aca3fb3e747507abc6e173cb6d5135
Chris <chris.gr.skinner@gmail.com>
2022-03-13 18:58:02 +0000

    trying to select individual entries from wiki api

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,51 @@
 // import MainContainer from './containers/MainContainer'
 import './App.css';
-import {getBattlefields, getWikiEntry, QuerySearch} from './services/BattlefieldService'
+import {getBattlefields, getWikiEntry } from './services/BattlefieldService'
 import {useState, useEffect} from 'react'
+
+
 
 function App() {
   const [battleData, setBattleData] = useState(null)
   const [wikiEntry, setWikiEntry] = useState(null)
+  const [wikiData, setWikiData] = useState(null)
   
+  const wikiBaseURL = 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch='
   
+  //   if (!battleData||!wikiEntry) {return (
+  //     <div>
+  //     Loading...
+  //   </div>
+  // ) } 
+  const battlefieldNodes = battleData.map((listItem) => {
+    return  (listItem.properties.name)
+  })
+
   useEffect ( () => {
     getBattlefields()
     .then(data => setBattleData(data))
     // .then(data => console.dir(data))
     
   } , [])
+  
+     const getWikiEntry = () => {
+      const result = fetch(wikiBaseURL + battlefieldNodes[0])
+      .then(res => res.json())
+  
+      setWikiData( result )
+  }
   useEffect ( () => {
     getWikiEntry()
     .then(data => setWikiEntry(data))
     .then(data => console.dir(data))
-
-
   } , [])
   
-  if (!battleData||!wikiEntry) {return (
-    <div>
-    Loading...
-  </div>
-) } 
-// if (!wikiEntry) {return (
-//     <div>
-//       Loading...
-//     </div>
-//   ) } 
+
+
+
+
+
   
-  const battlefieldNodes = battleData.map((listItem) => {
-    return  (listItem.properties.name)
-  })
   //needs to get an array from a dictionary
   // const wikiEntryNodes = wikiEntry.map((listItem) => {
   //   return  (listItem.query.search[0].title)
@@ -50,8 +60,8 @@ function App() {
       <p>Hello World</p>
       {console.log(battleData, "return battleData")}
       {console.log(wikiEntry, "return WikiEntry")}
-      {console.log (QuerySearch(), "return Queryseach")}
-      {battlefieldNodes}
+      {/* {console.log (QuerySearch, "return Queryseach")} */}
+      {battlefieldNodes[0]}
 
     </div>
   );
--- client/src/services/BattlefieldService.js
+++ client/src/services/BattlefieldService.js
@@ -1,4 +1,5 @@
-import {battleNodes} from '../'
+import {battleNodes} from '../App'
+
 const baseURL = 'http://127.0.0.1:5000/api/battlefields/';
 
 
@@ -10,34 +11,25 @@ export const getBattlefields = () => {
 
 };
 
-// using fixed URL to test - response currently null
-//ERROR - "Access to fetch at 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Battle%20of%20Barnet%201471&format=json' from origin 'http://localhost:3000' has been blocked by CORS policy: No 'Access-Control-Allow-Origin' header is present on the requested resource. If an opaque response serves your needs, set the request's mode to 'no-cors' to fetch the resource with CORS disabled."
-// SOLVED -> ADD &ORIGIN=* TO SEARCH COMMAND
-export const getWikiEntry = () => {
-    const result = fetch('https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=Battle%20of%20Barnet%201471&format=json&origin=* ')
-    .then(res => res.json())
 
-    return result
-}
+// export const QuerySearch = () => {var url = "https://en.wikipedia.org/w/api.php"; 
 
-export const QuerySearch = () => {var url = "https://en.wikipedia.org/w/api.php"; 
+// var params = {
+//     action: "query",
+//     list: "prefixsearch",
+//     pssearch: "battle of Adwalton",
+//     format: "json"
+// };
 
-var params = {
-    action: "query",
-    list: "prefixsearch",
-    pssearch: "battle of Adwalton",
-    format: "json"
-};
+// url = url + "?origin=*";
+// Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
 
-url = url + "?origin=*";
-Object.keys(params).forEach(function(key){url += "&" + key + "=" + params[key];});
-
-fetch(url)
-    .then(function(response){return response.json();})
-    .then(function(response) {
-        var pages = response.query.prefixsearch;
-        for (var page in pages) {
-            console.log( pages[page].title );
-        }
-    })
-    .catch(function(error){console.log(error);});}
+// fetch(url)
+//     .then(function(response){return response.json();})
+//     .then(function(response) {
+//         var pages = response.query.prefixsearch;
+//         for (var page in pages) {
+//             console.log( pages[page].title );
+//         }
+//     })
+//     .catch(function(error){console.log(error);});}
