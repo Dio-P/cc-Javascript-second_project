c0d598a64e15329650aa0d02a4f67f1d688ea6a5
Dionysios Papadopoulos <dionysiospapadopoulos@dionysioss-air.home>
2022-03-13 18:15:16 +0000

    this now seems to be working

--- client/src/App.js
+++ client/src/App.js
@@ -30,28 +30,9 @@ const purpleOptions = { color: 'purple' }
 
 
 const DisplayMap = ({ battlesData }) =>{
-  // const [battleData, setBattleData] = useState([])
   console.log("battlesData", battlesData);
 
   
-// const entities = battlesData;
-// const { battlesData }  = useContext(BattlesDataContext);
-// console.log("battlesData2", battlesData);
-// console.log("battlesData", battlesData);
-
-//   const changeYearValue = (value) => {
-//     setYearFilter(value);
-// }
-
-// const getYearOfBattle = (battlefield) => {
-//   const str = battlefield.name;
-//   const date = str.slice(str.length - 4).trim();
-//   return parseInt(date)
-// }
- 
-// const filteredData = entities.filter(battlefield => {
-//   return (getYearOfBattle(battlefield) <= yearFilter)
-// })
 
   return (
     <>
@@ -63,11 +44,10 @@ const DisplayMap = ({ battlesData }) =>{
       />
       {/* this under maps and displayes the battlefield. I tryed to have it in a different 
       component, but for some reason it dosent like it */}
-    {/* {  battlesData.map(entity => (
-      <Polygon pathOptions={purpleOptions} positions={entity.geojson.geometry.coordinates[0][0]
-        .map(coordinateArray=> coordinateArray.reverse())} />
+    {  battlesData.map(entity => (
+      <Polygon pathOptions={purpleOptions} positions={entity.geojson.geometry.coordinates[0][0]} />
     ))
-    }  */}
+    } 
     {/*the code bellow maps and renders the markers, this lives in a different component called MarkerElement  */}
     {battlesData.map((entity, index) => (
       <MarkerElement entity={entity} />
@@ -86,29 +66,10 @@ const DisplayMap = ({ battlesData }) =>{
 }
 
 function App() {
-  const [yearFilter, setYearFilter] = useState(2000);
-  const [battlesData, setBattlesData] = useState(null);
+  const [battlesData, setBattlesData] = React.useState(null);
+  const [activeBattle, setActiveBattle] = React.useState(null);
 
-  
-
-
-// let entities = async() => await fetch("http://localhost:5050/api/battles").then(res=> res.json())
-// let data = null
-
-// why???????
-
-// const entitiesTry = async() => {
-//   try{
-//   const data = await fetch("http://localhost:5050/api/battles");
-//   const dataArray = await data.json();
-//   console.log("dataArray", dataArray);
-//   return dataArray
-//   }catch(e){
-//     console.error(e)
-
-//   }
-// }
-// // let entities = ()=>entitiesTry()
+const purpleOptions = { color: 'purple' } 
 
 
   useEffect(() => {
@@ -116,38 +77,38 @@ function App() {
     .then(res => res.json())
     .then(res => {
       console.log("res", res);
-      setBattlesData(res);
+      const battlesReversed = res.map( battle => {
+        return { 
+          ...battle,
+          geojson: { 
+            ...battle.geojson,
+            geometry: {
+              ...battle.geojson.geometry,
+              coordinates: 
+              reverseCoordinates(battle.geojson.geometry.coordinates)
+            }
+           }
+        }
+      });
+      console.log("battlesReversed", battlesReversed);
+      setBattlesData(battlesReversed);
     })
     
   }, []);
 
-  // useEffect(() => {
-  //   const battleDataArray = async() =>{
-  //     try{
-  //       const initialData = await fetch("http://localhost:5050/api/battles");
-  //       let battleData = await initialData.json()
-  //       console.log("battleData inside", battleData);
-  //       return battleData
-  //     }catch(e){
-  //       console.error(e);
-  //     }
-  //   }
-  //   let preBattleData = async() => {
-  //     await battleDataArray().then(data=> {
-  //       console.log("data", data);
-  //       battleData.push(data); 
-  //       console.log("battleData In", battleData);
-  //       return data;
-  //     }).then(data=> {
-  //       setTimeout(() => {console.log("battleData[0]", battleData[0])}, 1000)
-  //     })
-      
-  //   }
-  //   preBattleData()
-    
-  //   console.log("battleData3", battleData);
-    
-  // }, []);
+  const reverseCoordinates = (array) => {
+    console.log("array to be reversed", array);
+    return array.map(firstInner=> {
+      console.log("firstInner", firstInner);
+      return firstInner.map( secondInner => {
+        console.log("secondInner", secondInner);
+        return secondInner.map( finalInner => {
+        console.log("finalInner", finalInner);
+          return finalInner.reverse()
+        })
+      })
+    })
+  }
 
   
   return (
