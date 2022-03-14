e6d99218f25f96d19df3473b30a9c9f0665fa006
Chris <chris.gr.skinner@gmail.com>
2022-03-14 22:34:50 +0000

    updating logic to clean strings - see test.js in main folder

--- server/db/cleanedseeds.js
+++ server/db/cleanedseeds.js
@@ -1,20 +1,34 @@
-// to be written
-// Batttlefield
+// // to be written
+// // Batttlefield
 
-// Geometry 
-        // -type
-        // -co-ord
-// Type (feature)
-// Properties
-        // -name
-
-// Wikidata
-//  Name
-//  Date
-//  Location
-//  Belligerents
-//  Causualties
-//  Result
-
-//  Wikilink
-//  games link to location [long, lat]
+// // Wikidata
+// //  Name
+// //  Date
+// //  Location
+// //  Belligerents
+// //  Causualties
+// //  Result
+// conflict
+// image
+// image_size
+// caption
+// partof
+// date
+// place
+// coordinates
+// result
+// combatant1
+// combatant2
+// commander1
+// commander2
+// strength1
+// strength2
+// casualties1
+// casualties2
+// lat1_deg
+// lon1_deg
+// position1
+// label1
+// lat2_deg
+// //  Wikilink
+// //  games link to location [long, lat]
--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ const app = express();
 import cors from 'cors'
 import fetch from 'node-fetch'
 import {MongoClient, ObjectID}  from 'mongodb';
+import wikiParser from 'wiki-infobox-parser';
 
 app.use(cors());
 import createRouter from './helpers/create_router.js';
@@ -13,38 +14,49 @@ const wikiBaseURL = 'https://en.wikipedia.org/w/api.php?action=query&list=search
 const wikiContentURL = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&rvsection=0&titles='
 
 
+  const stringParser = (string) => {
+    const stringArray = string.split(' ')
+    stringArray.pop()
+
+    return stringArray.join('_')
+
+  }
+ 
 MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
   .then((client) => {
     const db = client.db('HistoryApp');
     const battlefieldsCollection = db.collection('battlefields');
     const battlefieldsRouter = createRouter(battlefieldsCollection);
+    const wikiDataCollection = db.collection('WikiData')
+    const wikiDataRouter = createRouter(wikiDataCollection)
+    app.use('/api/WikiData', wikiDataRouter)
     app.use('/api/battlefields', battlefieldsRouter);
     app.get('/battledata/:id', (req,res) => {
       db.collection('battlefields').findOne({_id:ObjectID(req.params.id)})
       .then(resBattle => {
-        fetch(wikiBaseURL + resBattle.properties.name)
-        .then(wikiRes => wikiRes.json())
-        .then(wikiRes => {
-        console.log("wikiRes", wikiRes.query.search[0]);
-        fetch(wikiContentURL + wikiRes.query.search[0].title)
-        .then(wikiRes => wikiRes.json())
-        .then(data => res.json(data))
-  })
-
-
-      
+        const parsedString = stringParser(resBattle.properties.name)   
+        wikiParser(`${parsedString}`, function(err, result) {
+        if (err) {
+          console.error(err.message);
+        } else {
+          result => JSON.parse(result)
+          .then(console.log(result))
+          .then( result => db.collection('WikiData').insertOne({result}) )
+        }
       })
-      // fetch(**DATABASE**)
-      // .then(res => res.json())
-      // .then(res => {
-      //   console.log("res", res);
-      //   (res);
-    //   })
+    }
+    )
     })
   })
   .catch(console.err);
-
-
+ 
+  // wikiParser('battle_of_adwalton_moor', function(err, result) {
+  //   if (err) {
+  //     console.error(err.message);
+  //   } else {
+  //     console.log(result);
+  //   }
+  // });
 // if (battleData){
 //   // we are just fetching one [0]
 //   console.log("battledata:",battleData);
@@ -56,3 +68,21 @@ app.listen(5000, function () {
 });
 
   
+
+
+// MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
+//   .then((client) => {
+//     const db = client.db('HistoryApp');
+//     const battlefieldsCollection = db.collection('battlefields');
+//     const battlefieldsRouter = createRouter(battlefieldsCollection);
+//     app.use('/api/battlefields', battlefieldsRouter);
+//     app.get('/battledata/:id', (req,res) => {
+//       db.collection('battlefields').findOne({_id:ObjectID(req.params.id)})
+//       .then(resBattle => {
+//         fetch(wikiBaseURL + resBattle.properties.name)
+//         .then(wikiRes => wikiRes.json())
+//         .then(wikiRes => {
+//         console.log("wikiRes", wikiRes.query.search[0]);
+//         fetch(wikiContentURL + wikiRes.query.search[0].title)
+//         .then(wikiRes => wikiRes.json())
+//         .then(data => res.json(data))
