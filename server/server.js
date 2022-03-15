a6b27fe7eaef08879dcbbcbf4510fc6b21434ea7
Chris <chris.gr.skinner@gmail.com>
2022-03-15 12:06:37 +0000

    latest version of server

--- server/server.js
+++ server/server.js
@@ -1,76 +1,78 @@
 import express from 'express';
-
 const app = express();
 import cors from 'cors'
 import fetch from 'node-fetch'
 import {MongoClient, ObjectID}  from 'mongodb';
 import wikiParser from 'wiki-infobox-parser';
+import wikiCall from '../helpers/wikiCall'
+
 
 app.use(cors());
 import createRouter from './helpers/create_router.js';
 
 app.use(express.json());
-const wikiBaseURL = 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch='
-const wikiContentURL = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&rvsection=0&titles='
+// const wikiBaseURL = 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srsearch='
+// const wikiContentURL = 'https://en.wikipedia.org/w/api.php?action=query&prop=revisions&rvprop=content&format=json&rvsection=0&titles='
 
 
-  const stringParser = (string) => {
-    const stringArray = string.split(' ')
-    stringArray.pop()
+//   const stringParser = (string) => {
+//     const stringArray = string.split(' ')
+//     stringArray.pop()
 
-    return stringArray.join('_')
+//     return stringArray.join('_')
 
-  }
+//   }
  
-MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
-  .then((client) => {
-    const db = client.db('HistoryApp');
-    const battlefieldsCollection = db.collection('battlefields');
-    const battlefieldsRouter = createRouter(battlefieldsCollection);
-    const wikiDataCollection = db.collection('WikiData')
-    const wikiDataRouter = createRouter(wikiDataCollection)
-    app.use('/api/WikiData', wikiDataRouter)
-    app.use('/api/battlefields', battlefieldsRouter);
-    app.get('/battledata/:id', (req,res) => {
-      db.collection('battlefields').findOne({_id:ObjectID(req.params.id)})
-      .then(resBattle => {
-        const parsedString = stringParser(resBattle.properties.name)   
-        wikiParser(`${parsedString}`, function(err, result) {
-          console.log("result from parser",result);
-        if (result) {
-          console.log("just inside if",result)
-          const parser = JSON.parse(result)
-          console.log("JSONparser:",parser );
-          db.collection('WikiData').insertOne(parser) 
-        }
-        if (err) {
-          console.error(err.message);
-        }
-        }
-      )
-    }
-    )
-    })
-  })
-  .catch(console.err);
- 
-  // wikiParser('battle_of_adwalton_moor', function(err, result) {
-  //   if (err) {
-  //     console.error(err.message);
-  //   } else {
-  //     console.log(result);
-  //   }
-  // });
-// if (battleData){
-//   // we are just fetching one [0]
-//   console.log("battledata:",battleData);
+// MongoClient.connect('mongodb://127.0.0.1:27017', { useUnifiedTopology: true })
+//   .then((client) => {
+//     const db = client.db('HistoryApp');
+//     const battlefieldsCollection = db.collection('battlefields');
+//     const battlefieldsRouter = createRouter(battlefieldsCollection);
+//     const wikiDataCollection = db.collection('WikiData')
+//     const wikiDataRouter = createRouter(wikiDataCollection)
+//     app.use('/api/WikiData', wikiDataRouter)
+//     app.use('/api/battlefields', battlefieldsRouter);
+//     app.get('/battledata/:id', (req,res) => {
+//       db.collection('battlefields').findOne({_id:ObjectID(req.params.id)})
+//       .then(resBattle => {
+//         const parsedString = stringParser(resBattle.properties.name)   
+//         wikiParser(`${parsedString}`, function(err, result) {
+//           console.log("result from parser",result);
+//         if (result) {
+//           console.log("just inside if",result)
+//           const parser = JSON.parse(result)
+//           console.log("JSONparser:",parser );
+//           db.collection('WikiData').insertOne(parser) 
+//         }
+//         if (err) {
+//           console.error(err.message);
+//         }
+//         }
+//       )
+//     }
+//     )
+//     })
+//   })
+//   .catch(console.err);
   
+  
+  app.listen(5050, function () {
+    console.log(`Listening on port ${ this.address().port }`);
+  });
+  app.post('./wikiData', wikiCall);
 
-
-app.listen(5000, function () {
-  console.log(`Listening on port ${ this.address().port }`);
-});
-
+  
+   // wikiParser('battle_of_adwalton_moor', function(err, result) {
+   //   if (err) {
+   //     console.error(err.message);
+   //   } else {
+   //     console.log(result);
+   //   }
+   // });
+ // if (battleData){
+ //   // we are just fetching one [0]
+ //   console.log("battledata:",battleData);
+   
   
 
 
@@ -90,3 +92,4 @@ app.listen(5000, function () {
 //         fetch(wikiContentURL + wikiRes.query.search[0].title)
 //         .then(wikiRes => wikiRes.json())
 //         .then(data => res.json(data))
+export default app
--- test.js
+++ test.js
@@ -20,3 +20,107 @@ console.log(finalObject);
 // return res}
 
 // stringCleaner(wikiData)
+
+//Tags:
+// partof
+// place
+
+// _id
+// :
+// 6230749bea2337e623267fb4
+// conflict
+// :
+// "Battle of Adwalton Moor"
+// image
+// :
+// "Battle Plaque at Adwalton Moor - geograph.org.uk - 1069183.jpg"
+// image_size
+// :
+// "300"
+// caption
+// :
+// "West Yorkshire and Adwalton"
+// partof
+// :
+// "the First English Civil War"
+// date
+// :
+// "{{start date and age|1643|06|30|df=y}}"
+// place
+// :
+// "Adwalton, Yorkshire"
+// coordinates
+// :
+// "{{Coord|53.751|-1.664|type:event_region:GB|display=title}}"
+// result
+// :
+// "Royalist Victory"
+// combatant1
+// :
+// "{{flagicon image|Royal Standard of England (1603–1689).svg|size=22px}}..."
+// combatant2
+// :
+// "22px Parliamentarians"
+// commander1
+// :
+// "Earl of Newcastle"
+// commander2
+// :
+// "Lord FairfaxSir Thomas FairfaxMajor General Gifford"
+// strength1
+// :
+// "4,000 foot soldiers{{sfn|MacKenzie|2020}}3,000 horse"
+// strength2
+// :
+// "6,000 foot soldiers{{sfn|MacKenzie|2020}}1,500 horseLocal countrymen"
+// casualties1
+// :
+// "200 killed{{sfn|MacKenzie|2020}}300 wounded"
+// casualties2
+// :
+// "500 killed{{sfn|MacKenzie|2020}}1,500 captured"
+// lat1_deg
+// :
+// "53.751"
+// lon1_deg
+// :
+// "-1.664"
+// position1
+// :
+// "right"
+// label1
+// :
+// "Adwalton"
+// lat2_deg
+// :
+// "53.8"
+// lon2_deg
+// :
+// "-1.75"
+// position2
+// :
+// "left"
+// label2
+// :
+// "Bradford"
+// lat3_deg
+// :
+// "53.799722"
+// lon3_deg
+// :
+// "-1.549167"
+// label3
+// :
+// "Leeds"
+// lat4_deg
+// :
+// "53.722642"
+// lon4_deg
+// :
+// "-1.614974"
+// position4
+// :
+// "right"
+// label4
+// :
+// "Batley"
